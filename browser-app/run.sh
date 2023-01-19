7ac7723e3333b2b83f09a00e094fd1e06fe2546f
Derek Smith <derek@farscapian.com>
2023-01-19 09:55:25 -0500

    Improve browser-app deployment.

--- browser-app/run.sh
+++ browser-app/run.sh
@@ -3,7 +3,7 @@
 set -e
 cd "$(dirname "$0")"
 
-# read in the variable from the files in ./
+# shellcheck source=./env
 source ./env
 
 OUTPUT_DIR="$(pwd)/www-root"
@@ -30,27 +30,21 @@ if docker ps -a | grep -q "$REPO_NAME"; then
     docker system prune -f
 fi
 
-# build the dockerfile.
+# pull the base image from dockerhub and build the ./Dockerfile.
 docker pull node:latest
 docker build --build-arg GIT_REPO_URL="$REPO_URL" --build-arg VERSION="$GIT_TAG" -t browser-app:"$GIT_TAG" .
 
-################
-
 # If the existing output directory exists, we delete it so we can get fresh files.
 if [ -d "$OUTPUT_DIR" ]; then
-    sudo rm -rf "$OUTPUT_DIR"
+    rm -rf "$OUTPUT_DIR"
     echo "INFO: Your existing output files have been deleted."
 fi
 
 echo "Creating '$OUTPUT_DIR'."
 mkdir "$OUTPUT_DIR"
 
-# copy the files from the build process to OUTPUT_DIR
-docker run -it -v "$OUTPUT_DIR":/output --name browser-app "$REPO_NAME":"$GIT_TAG"
-
-# TODO remove; see if we can get UID mapped
-sudo chown -R "$USER:$USER" "$OUTPUT_DIR"
-
-docker system prune -f >> /dev/null
+# run the image, which by default copies the build output to /output in the container
+# /output is mounted to a local host directory.
+docker run -it --rm --user "$UID:$UID" -v "$OUTPUT_DIR":/output --name browser-app "$REPO_NAME":"$GIT_TAG"
 
 echo "Your static files for Clams browser app can be found at ${OUTPUT_DIR}"
