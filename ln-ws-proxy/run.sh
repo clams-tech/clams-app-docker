a9ed8142a7b1b881a6cbcb42325f7cb5f230bc42
Derek Smith <derek@farscapian.com>
2023-02-07 21:16:24 -0500

    Separate build/run scripts for ln-ws-proxy.

--- ln-ws-proxy/run.sh
+++ ln-ws-proxy/run.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-
 set -ex
 cd "$(dirname "$0")"
 
-# shellcheck source=./env
-source ./env
+# run the build script.
+./build.sh
 
-if docker ps | grep -q "$REPO_NAME"; then
-    docker kill "$REPO_NAME"
-fi
-
-if docker ps -a | grep -q "$REPO_NAME"; then
-    docker system prune -f
-fi
-
-# build the dockerfile.
-docker build --build-arg GIT_REPO_URL="$REPO_URL" --build-arg VERSION="$GIT_TAG" -t "$REPO_NAME":"$GIT_TAG" .
+. ./env
 
 # run the service.
 docker run -d \
-    --name "$REPO_NAME" \
+    --name ln-ws-proxy \
     -p 127.0.0.1:3000:3000 \
-    "$REPO_NAME":"$GIT_TAG"
+    ln-ws-proxy:"$GIT_TAG"
