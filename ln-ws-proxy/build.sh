b0a3358cba7561792c2b8af520cb328af55c8ecc
farscapian <derek@farscapian.com>
2023-02-11 19:20:53 -0500

    Various updates including separating build/run scripts and updating to latest version.  (#4)
    * Documentation updates.
    
    * Separate build/run process.
    
    * Move folder creation.
    
    * Bump browser-app version to 1.4.0
    
    * Separate build/run scripts for ln-ws-proxy.
    
    * Bump version for ln-ws-proxy.
    
    * Remove debug flag.
    
    * Fix link error.

---
+++ ln-ws-proxy/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+set -e
+cd "$(dirname "$0")"
+
+# shellcheck source=./env
+source ./env
+
+# build the dockerfile.
+docker build --build-arg GIT_REPO_URL="$REPO_URL" --build-arg VERSION="$GIT_TAG" -t ln-ws-proxy:"$GIT_TAG" .
