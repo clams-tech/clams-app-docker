6fa4cf2e427340d973fb03a930bb96705f6cc067
Derek Smith <derek@farscapian.com>
2023-03-04 14:37:40 -0500

    Improve global up/down/reset scripts.

--- get_rune.sh
+++ get_rune.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-RUNE_TYPE=
+RUNE_TYPE="read-only"
+SESSION_ID=
 
 # grab any modifications from the command line.
 for i in "$@"; do
     case $i in
         --type=*)
             RUNE_TYPE="${i#*=}"
+            shift
+        ;;
+        --session-id=*)
+            SESSION_ID="${i#*=}"
             shift
         ;;
         *)
@@ -19,21 +24,27 @@ for i in "$@"; do
 done
 
 if [ -z "$RUNE_TYPE" ]; then
-    echo "ERROR: rune type must be specified."
+    echo "ERROR: rune typ e must be specified. Use --type=[read-only|clams|admin]"
     exit 1
 fi
 
-echo "Please enter the Clams rune command you copied from the app:  "
-read -r SESSION_ID
-
-source ./.env
-
-if docker ps -a | grep -q clams-clightning; then
-    COMMAND_TO_EXECUTE="docker exec -it -u $UID:$UID clams-clightning lightning-cli --network $BTC_CHAIN commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"rate=60\"]]'"
-
-    ADMIN_RUNE=$(eval "$COMMAND_TO_EXECUTE" | jq -r '.rune')
-    echo "Rune: $ADMIN_RUNE"
-else
-    echo "ERROR: Cannot find the clightning container. Did you run it?"
+if [ -z "$SESSION_ID" ]; then
+    echo "ERROR: The SESSION_ID was not set."
     exit 1
 fi
+
+# TODO validate SESSION ID INPUT.
+
+RUNE=
+if [ "$RUNE_TYPE" = admin ]; then
+    RUNE=$(bash -c "./lightning-cli.sh commando-rune restrictions='[[\"id=${SESSION_ID}\"], [\"rate=60\"]]'")
+elif [ "$RUNE_TYPE" = read-only ]; then
+    RUNE=$(bash -c "./lightning-cli.sh commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=waitanyinvoice\",\"method=waitinvoice\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
+elif [ "$RUNE_TYPE" = clams ]; then
+    RUNE=$(bash -c "./lightning-cli.sh commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=pay\",\"method=keysend\",\"method=invoice\",\"method=waitanyinvoice\",\"method=waitinvoice\", \"method=signmessage\", \"method^bkpr-\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
+    
+else     echo "ERROR: invalid RUNE_TYPE."
+    exit 1
+fi
+
+echo "$RUNE" | jq -r '.rune'
