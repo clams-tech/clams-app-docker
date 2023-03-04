401a7693cd2d759f44bda51e76b57c08a4bce720
Derek Smith <derek@farscapian.com>
2023-03-04 14:34:50 -0500

    Improve get_rune.sh

--- get_node_uri.sh
+++ get_node_uri.sh
@@ -3,18 +3,8 @@
 set -e
 cd "$(dirname "$0")"
 
+. ./defaults.env
+. ./.env
 
-source ./.env
-
-NODE_PUBKEY=
-if docker ps -a | grep -q clams-clightning; then
-    NODE_PUBKEY=$(docker exec -it -u "$UID:$UID" clams-clightning lightning-cli --network "$BTC_CHAIN" getinfo | jq -r '.id')
-else
-    echo "ERROR: Cannot find the clightning container. Did you run it?"
-    exit 1
-fi
-
-source ./backend/.env
-
-echo "Your node connection details are:"
-echo "core-lightning websocket \"Direct Connection (ws)\": $NODE_PUBKEY@$WEBSOCKET_BIND_ADDR:$WEBSOCKET_PORT"
+NODE_PUBKEY=$(docker exec -it -u "$UID:$UID" clams-clightning lightning-cli --network "$BTC_CHAIN" getinfo | jq -r '.id')
+echo "$NODE_PUBKEY@$CLAMS_FQDN:$CLIGHTNING_WEBSOCKET_EXTERNAL_PORT"
