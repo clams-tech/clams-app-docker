87f2cfd675fb8f760c5a5b94e829cf19fdd8451e
Derek Smith <derek@farscapian.com>
2023-05-03 22:51:09 -0400

    Added Install script.

--- install.sh
+++ install.sh
@@ -2,5 +2,29 @@
 
 sudo apt update
 
+# needed by tehse scripts
 sudo apt install -y jq dnsutils
 
+
+# the rest is needed for docker to work
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker ubuntu
