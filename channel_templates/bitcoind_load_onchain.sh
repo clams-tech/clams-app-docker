54bf582db4bc9582f97af37920bd6aee305031a6
daGoodenough <dgoodenough@proton.me>
2023-05-05 12:31:52 -0700

    More changes to bitcoind_load_onchain

--- channel_templates/bitcoind_load_onchain.sh
+++ channel_templates/bitcoind_load_onchain.sh
@@ -6,44 +6,61 @@
 
 set -ex
 
-
 bcli() {
-    bash -c "../bitcoin-cli.sh $@"
+    "$(dirname "$0")/../bitcoin-cli.sh" "$@"
 }
+
+
+#set this var here for testing
+BTC_CHAIN=regtest
 
 
 #first we need to check if the prism wallet exists in the wallet dir
 if [[ $(bcli listwalletdir) == *'"name": "prism"'* ]]; then
     #if it exists, then check if it's loaded
-    if bcli listwallets | grep -q "test"; then
+    if bcli listwallets | grep -q "prism"; then
         echo "Wallet is already loaded"
     else
-        bcli loadwallet "prism.dat"
+        bcli loadwallet "prism"
     fi
 else
     #create walllet if it does not already exist
-    bcli createwallet wallet_name="prism" load_on_startup=true #not working, returning error code: -1
+    bcli createwallet prism
 fi
 
 
 #create address controlled by our wallet
-BTC_ADDRESS=$(bcli getnewaddress) #must request wallet RPC through /wallet/<filename> uri-path).
-                                        # try adding "-rpcwallet=<filename>" option to bitcoin-cli command line
+BTC_ADDRESS=$(bcli getnewaddress)
+# The above command will only work if only one wallet it loaded
+#TODO: specify which wallet to target
+#error message:
+#Wallet file not specified (must request wallet RPC through /wallet/<filename> uri-path).
+#Try adding "-rpcwallet=<filename>" option to bitcoin-cli command line.
+echo "BTC_ADDRESS: $BTC_ADDRESS"
+
 
 #if regtest, check that there are at least 101 blocks
-if [ "$BTC_CHAIN" = regtest]; then
+if [ "$BTC_CHAIN" == regtest ]; then
     num_blocks=$(bcli getblockchaininfo | jq '.blocks')
 
     if ((num_blocks < 101)); then
-        bcli generatetoaddress 101 $BTC_ADDRESS
+        CMD="$(dirname "$0")/../bitcoin-cli.sh generatetoaddress 101 $BTC_ADDRESS"
+        echo "Please execute the following command:"
+        echo "$CMD"
     fi
 
     #check wallet balance
     wallet_balance=$(bcli getwalletinfo | jq '.balance')
     if ((wallet_balance < 5)); then
-        bcli generatetoaddress 1 $BTC_ADDRESS
+        CMD="$(dirname "$0")/../bitcoin-cli.sh generatetoaddress 1 $BTC_ADDRESS"
+        echo "Please execute the following command:"
+        echo "$CMD"
     fi
+else
+    echo "Not in regtest"
 fi
+
+
 
 
 
