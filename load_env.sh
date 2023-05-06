7a2c31eccc5afa81b0f98d182922f0f6e02b139a
Derek Smith <derek@farscapian.com>
2023-05-06 17:50:01 -0400

    Add env file stubbing and error checking.

--- load_env.sh
+++ load_env.sh
@@ -3,6 +3,7 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+DOCKER_HOST=
 
 # read in ./active_env then source the file if it exists. export variables.
 ACTIVE_ENV=$(cat ./active_env | head -n1 | awk '{print $1;}')
@@ -10,11 +11,22 @@ ACTIVE_ENV=$(cat ./active_env | head -n1 | awk '{print $1;}')
 ENV_FILE="./environments/$ACTIVE_ENV"
 
 if [ ! -f "$ENV_FILE" ]; then
-    echo "ERROR: the env file does not exist. Check ./active_env and ensure the env file exists in ./environments/"
+    cat > "$ENV_FILE" << EOF
+DOCKER_HOST=ssh://ubuntu@domain.tld
+DOMAIN_NAME=domain.tld
+ENABLE_TLS=true
+EOF
+
+    echo "WARNING: The env file '$ENV_FILE' didn't exist, so we stubbed it out. Go update it!"
     exit 1
 fi
 
 source "$ENV_FILE"
+
+if [ "$DOMAIN_NAME" = "domain.tld" ]; then
+    echo "ERROR: Hey, you didn't update your env file!"
+    exit 1
+fi
 
 export DOCKER_HOST="$DOCKER_HOST"
 export DOMAIN_NAME="$DOMAIN_NAME"
