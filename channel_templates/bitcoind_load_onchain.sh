8d3e552ed9718d539d80b43167bf0c379515aa81
nick shoup <shoupnb@gmail.com>
2023-05-06 18:11:32 -0600

    fix for generating new address and generating to address

--- channel_templates/bitcoind_load_onchain.sh
+++ channel_templates/bitcoind_load_onchain.sh
@@ -32,13 +32,12 @@ echo "$WALLET_NAME wallet initialized"
 if [ "$(echo "$WALLET_BALANCE < 5" | bc -l) " -eq 1 ]; then
 #create address controlled by our wallet
 #bcrt1qqd7yn0wll8vx0lxe2sdh224dqathne0f3fefa2
-#BTC_ADDRESS=$(bcli getnewaddress | xargs)
-BTC_ADDRESS=bcrt1qn0ej8qzxwmmgxkcagd80rmfyfnpdak94v8zj26
-#$(bcli getnewaddress | xargs)
-echo "BTC_ADDRESS: $BTC_ADDRESS"
+BTC_ADDRESS=$(bcli getnewaddress)
+CLEAN_BTC_ADDRESS=$(echo -n "$BTC_ADDRESS" | tr -d '\r')
+echo "BTC_ADDRESS: $CLEAN_BTC_ADDRESS"
 echo "Make sure that the above address is created from your wallet"
     if [ "$BTC_CHAIN" == regtest ]; then
-        bcli generatetoaddress 101 "$BTC_ADDRESS"
+        bcli generatetoaddress 101 $CLEAN_BTC_ADDRESS
         echo "101 blocks mined to $WALLET_NAME"
     else 
         echo "ERROR: You are on $BTC_CHAIN and cannot generate coins to your wallet. Figure out how to get some onchain funds"
