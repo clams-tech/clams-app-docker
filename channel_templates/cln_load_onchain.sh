4a035e5482b77cc4d66a1aefb9d47d48d0d6c2f0
Derek Smith <derek@farscapian.com>
2023-05-07 12:31:07 -0400

    Updates to cln_load_onchain

--- channel_templates/cln_load_onchain.sh
+++ channel_templates/cln_load_onchain.sh
@@ -3,13 +3,10 @@
 # the purpose of this script is to ensure that all CLN nodes have on-chain funds.
 # assumes bitcoind has a loaded wallet with spendable funds
 
-
 set -e
 
-
 # fund each cln node
-for ((i=0; i<CLN_COUNT; i++)); do
-    BALANCE=$(lncli --id="$i" bkpr-listbalances | jq -r '.accounts[0].balances[0].balance_msat[:-4] | tonumber')
+for ((CLN_ID=0; CLN_ID<CLN_COUNT; CLN_ID++)); do
 
     #check that we have at least 5 btc
     if [ "$BALANCE" -ge 500000000000 ]; then
@@ -17,7 +14,19 @@ for ((i=0; i<CLN_COUNT; i++)); do
     else
         echo "Insufficient funds. Sending 5 btc to cln-$i"
         CLN_ADDR=$(lncli --id="$i" newaddr | jq -r '.bech32')
-        bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
+    echo "Insufficient funds. Sending 5 btc to cln-$CLN_ID"
         bcli -generate 1 > /dev/null
+
+    if [ "$CLN_ID" = 1 ]; then
+        bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
+        bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
+        bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
+        bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
+        bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
+    else
+        bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
     fi
+
+    bcli -generate 10 > /dev/null
+
 done
