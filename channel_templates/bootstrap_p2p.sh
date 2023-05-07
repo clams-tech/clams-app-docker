d9be87b9886e62f2532ddc02907e0290ac461350
Derek Smith <derek@farscapian.com>
2023-05-07 12:36:58 -0400

    Update to bootstrap p2p network.

--- channel_templates/bootstrap_p2p.sh
+++ channel_templates/bootstrap_p2p.sh
@@ -4,20 +4,23 @@ set -e
 
 # iterate through each node and it open 4 P2P connections to its neigh neighbor.
 for ((NODE_ID=0; NODE_ID<CLN_COUNT; NODE_ID++)); do
-    peers=($(lncli --id="${NODE_ID}" listpeers 2>/dev/null | jq -r '.peers'))
-    if [ ${#peers[@]} -ge 4 ]; then
-        echo "Node ${NODE_ID} is connected to ${#peers[@]} peers"
-        continue
-    else
-        for i in {1..4}; do
-            NODE_PLUS_I=$((NODE_ID+i))
-            NODE_MOD_COUNT=$((NODE_PLUS_I%4))
 
-            if [ "$NODE_MOD_COUNT" != "$NODE_ID" ]; then
-                # Now open a p2p connection
-                NEXT_NODE_PUBKEY=$(lncli --id=$NODE_MOD_COUNT getinfo | jq -r '.id')
-                lncli --id="$NODE_ID" connect "$NEXT_NODE_PUBKEY" "cln-$NODE_MOD_COUNT" 9735 > /dev/null
-            fi
-        done
+    # first we should check if the node has any peers already
+    NODE_PEER_COUNT="$(lncli --id=${NODE_ID} listpeers | jq -r '.peers | length')"
+    if (( "$NODE_PEER_COUNT" >= 4 )); then
+        echo "Node $NODE_ID has $NODE_PEER_COUNT peers."
+        continue
     fi
+
+    for i in {1..4}; do
+        NODE_PLUS_I=$((NODE_ID+i))
+        NODE_MOD_COUNT=$((NODE_PLUS_I%4))
+
+        if [ "$NODE_MOD_COUNT" != "$NODE_ID" ]; then
+            # Now open a p2p connection
+            NEXT_NODE_PUBKEY=$(lncli --id=$NODE_MOD_COUNT getinfo | jq -r '.id')
+            lncli --id="$NODE_ID" connect "$NEXT_NODE_PUBKEY" "cln-$NODE_MOD_COUNT" 9735 > /dev/null
+        fi
+    done
+
 done
