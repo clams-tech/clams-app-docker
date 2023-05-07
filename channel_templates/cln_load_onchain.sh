3265fd2522f079cac680a8b10c8984e5dc901320
Derek Smith <derek@farscapian.com>
2023-05-07 12:36:16 -0400

    Updates to cln_load_onchain.

--- channel_templates/cln_load_onchain.sh
+++ channel_templates/cln_load_onchain.sh
@@ -7,15 +7,17 @@ set -e
 
 # fund each cln node
 for ((CLN_ID=0; CLN_ID<CLN_COUNT; CLN_ID++)); do
+    BALANCE_MSAT_STR=$(lncli --id="$CLN_ID" bkpr-listbalances | jq -r '.accounts[0].balances[0].balance_msat')
+    BALANCE_MSAT=${BALANCE_MSAT_STR%msat}
 
     #check that we have at least 5 btc
-    if [ "$BALANCE" -ge 500000000000 ]; then
-        echo "cln-$i has sufficient funds: $BALANCE mSats"
-    else
-        echo "Insufficient funds. Sending 5 btc to cln-$i"
-        CLN_ADDR=$(lncli --id="$i" newaddr | jq -r '.bech32')
+    if (("$BALANCE_MSAT" > 500000000000 )); then
+        echo "INFO: cln-$CLN_ID has sufficient funds: $BALANCE_MSAT mSats"
+        continue
+    fi
+
     echo "Insufficient funds. Sending 5 btc to cln-$CLN_ID"
-        bcli -generate 1 > /dev/null
+    CLN_ADDR=$(lncli --id="$CLN_ID" newaddr | jq -r '.bech32')
 
     if [ "$CLN_ID" = 1 ]; then
         bcli sendtoaddress "$CLN_ADDR" 5 > /dev/null
