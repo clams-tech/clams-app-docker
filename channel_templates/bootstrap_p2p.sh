6666c6034360f976ea23107d518ad60f4f4dc0d9
daGoodenough <dgoodenough@proton.me>
2023-05-06 21:52:37 -0700

    only tries to connect to peers if there aren't any

--- channel_templates/bootstrap_p2p.sh
+++ channel_templates/bootstrap_p2p.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 # iterate through each node and it open 4 P2P connections to its neigh neighbor.
 for ((NODE_ID=0; NODE_ID<CLN_COUNT; NODE_ID++)); do
-    echo "Current ID: ${NODE_ID}"
-    for i in {1..4}; do
-        NODE_PLUS_I=$((NODE_ID+i))
-        NODE_MOD_COUNT=$((NODE_PLUS_I%4))
+    peers=($(lncli --id="${NODE_ID}" listpeers 2>/dev/null | jq -r '.peers'))
+    if [ ${#peers[@]} -ge 4 ]; then
+        echo "Node ${NODE_ID} is connected to ${#peers[@]} peers"
+        continue
+    else
+        for i in {1..4}; do
+            NODE_PLUS_I=$((NODE_ID+i))
+            NODE_MOD_COUNT=$((NODE_PLUS_I%4))
 
-        if [ "$NODE_MOD_COUNT" != "$NODE_ID" ]; then
-            # Now open a p2p connection
-            NEXT_NODE_PUBKEY=$(lncli --id=$NODE_MOD_COUNT getinfo | jq -r '.id')
-            lncli --id=$NODE_ID connect "$NEXT_NODE_PUBKEY" "cln-$NODE_MOD_COUNT" 9735
-        fi
-    done
+            if [ "$NODE_MOD_COUNT" != "$NODE_ID" ]; then
+                # Now open a p2p connection
+                NEXT_NODE_PUBKEY=$(lncli --id=$NODE_MOD_COUNT getinfo | jq -r '.id')
+                lncli --id="$NODE_ID" connect "$NEXT_NODE_PUBKEY" "cln-$NODE_MOD_COUNT" 9735 > /dev/null
+            fi
+        done
+    fi
 done
