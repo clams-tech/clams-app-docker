a3f2e33f08569d2c451c79c1ad2b380553dca51d
Derek Smith <derek@farscapian.com>
2023-05-08 08:31:56 -0400

    Fix admin rune bug.

--- get_rune.sh
+++ get_rune.sh
@@ -26,22 +26,19 @@ for i in "$@"; do
     esac
 done
 
-if [ -z "$SESSION_ID" ]; then
-    echo "ERROR: SESSION_ID must be specified."
-    exit 1
-fi
-
 RUNE_JSON=
 if [ "$RUNE_TYPE" = admin ]; then
-    RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=${SESSION_ID}\"], [\"rate=60\"]]'")
+    # if no session ID is specified, we return an ADMIN RUNE
+    # TODO input validation on the session id.
+    if [ -n "$SESSION_ID" ]; then
+        RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=${SESSION_ID}\"], [\"rate=60\"]]'")
+    else
+        RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune")
+    fi 
 elif [ "$RUNE_TYPE" = read-only ]; then
     RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=waitanyinvoice\",\"method=waitinvoice\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
 elif [ "$RUNE_TYPE" = clams ]; then
     RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=pay\",\"method=keysend\",\"method=invoice\",\"method=waitanyinvoice\",\"method=waitinvoice\", \"method=signmessage\", \"method^bkpr-\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
-    
-else
-    echo "ERROR: invalid RUNE_TYPE."
-    exit 1
 fi
 
 RUNE=$(echo "$RUNE_JSON" | jq -r '.rune')
